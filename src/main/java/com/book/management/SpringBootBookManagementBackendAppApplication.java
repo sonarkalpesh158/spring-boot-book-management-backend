a011ca6495b57dda8c07270845e2cea77207c44a
sonar <sonaraklpesh1998@gmail.com>
2024-12-28 13:52:03 +0530

    Initial commit

---
+++ src/main/java/com/book/management/SpringBootBookManagementBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.book.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookManagementBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBookManagementBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book/management/SpringBootBookManagementBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBookManagementBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
